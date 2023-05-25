1c5a14aca0f6f01391cab89eb5e1893f670f4757
Zeb <zeb.7237@gmail.com>
2023-05-25 16:43:48 +0800

    1.调整目录结构

--- admin/serve/internal/logic/servelogic.go
+++ admin/serve/internal/logic/servelogic.go
@@ -25,6 +25,7 @@ func NewServeLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ServeLogic 
 
 func (l *ServeLogic) Serve(req *types.Request) (resp *types.Response, err error) {
 	// todo: add your logic here and delete this line
-
-	return
+   return &types.Response{
+        Message: "Hello go-zero",
+    }, nil
 }
