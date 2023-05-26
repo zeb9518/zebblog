c3e8dab15a966dcade53bd89aa8f935da52171a3
Zeb <zeb.7237@gmail.com>
2023-05-26 09:14:41 +0800

    1.调整登录页面

--- admin/web/src/pages/login/index.tsx
+++ admin/web/src/pages/login/index.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import Footer from '@/components/Footer';
-import Logo from '@/assets/logo.svg';
 import LoginForm from './form';
-import LoginBanner from './banner';
 import styles from './style/index.module.less';
 
 function Login() {
@@ -12,15 +10,6 @@ function Login() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>
-        <Logo />
-        <div className={styles['logo-text']}>Arco Design Pro</div>
-      </div>
-      <div className={styles.banner}>
-        <div className={styles['banner-inner']}>
-          <LoginBanner />
-        </div>
-      </div>
       <div className={styles.content}>
         <div className={styles['content-inner']}>
           <LoginForm />
